0c9f31904cf1898368e4c93a16d8d7a601146fb4
Russell Owen <rowen@uw.edu>
2018-08-01 13:01:53 -0700

    Remove python 2 support

--- tests/test_kron.py
+++ tests/test_kron.py
@@ -20,8 +20,6 @@
 # the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
-from __future__ import print_function
-from builtins import range
 import math
 import unittest
 import sys
